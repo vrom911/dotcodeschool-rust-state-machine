023beca18dc791806d235df1e496b20c69c0318d
Veronika Romashkina <vrom911@gmail.com>
2025-05-04 23:14:40 +0100

    Implement tight coupling of Configs

--- src/balances.rs
+++ src/balances.rs
@@ -1,8 +1,7 @@
 use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
 
-pub trait Config {
-	type AccountId: Ord + Clone;
+pub trait Config: crate::system::Config {
 	type Balance: Zero + CheckedSub + CheckedAdd + Copy;
 }
 
@@ -59,8 +58,12 @@ impl<T: Config> Pallet<T> {
 #[cfg(test)]
 mod tests {
 	struct TestConfig;
-	impl super::Config for TestConfig {
+	impl crate::system::Config for TestConfig {
 		type AccountId = String;
+		type BlockNumber = u32;
+		type Nonce = u32;
+	}
+	impl super::Config for TestConfig {
 		type Balance = u128;
 	}
 	#[test]
